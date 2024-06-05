3960e33b7055188e0e5ed5f33c193fd8d2f1b3ff
Lionel <nkabisto@gmail.com>
2024-06-05 16:53:02 +0200

    First Query - Hello World

---
+++ server.js
@@ -0,0 +1,23 @@
+/*To handle GraphQL queries, we need a schema that defines the Query type, and we need an API root with a function called a
+"resolver" for each API endpoint. For an API tha just returns "Hello world!", we can put this code in a fie named server.js
+*/
+
+var {graphql, buildSchema} = require("graphql")
+
+// Construct a schema, using a GraphQL schema language
+var schema = buildSchema(`
+    type Query(
+        type Query{
+            hello: String
+        }
+    )
+`)
+
+// Run the GraphQL query '{hello}' and print out the response
+graphql({
+    schema,
+    source: "{hello}",
+    rootValue
+}).then(response => {
+    console.log(response)
+})
