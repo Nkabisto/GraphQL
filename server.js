521ce39be99611071fa6619fdf4177944a09202f
lionel <nkabisto@gmail.com>
2024-06-20 11:17:20 +0200

    Passing Arguments

--- server.js
+++ server.js
@@ -2,25 +2,21 @@ var express = require("express")
 var { createHandler } = require("graphql-http/lib/use/express")
 var { buildSchema } = require("graphql")
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+// Construct a schema, using GraphQL schema language 
+var schema = buildSchema(/* GraphQL 8 */`
 	type Query {
-		quoteOfTheDay: String
-		random: Float!
-		rollThreeDice: [Int]
+	rollDice(numDice: Int!, numSides: Int): [Int]
 	}
 `)
 
 // The root provides a resolver function for each API endpoint
 var root = {
-	quoteOfTheDay(){
-		return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within"
-	},
-	random(){
-		return Math.random()
-	},
-	rollThreeDice(){
-		return [1,2,3].map(_ => 1 + Math.floor(Math.random() * 6))
+		rollDice({ numDice, numSides}) {
+		var output = []
+		for (var i = 0; i < numDice; i++) {
+			output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+		}
+		return output
 	},
 }
 
@@ -32,6 +28,6 @@ app.all(
 		rootValue: root,
 	})
 )
+
 app.listen(4000)
 console.log("Running a GraphQL API server at localhost:4000/graphql")
-
