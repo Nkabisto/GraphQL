12ae8c4e021f2f9c87d6c046488fa54c2e49222f
Lionel <nkabisto@gmail.com>
2024-06-20 02:50:21 +0200

    Basic Types

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-/** Let's modify our "hello world" example so that it's an API sever rather tan a script that runs a single query. We can use the 'express' module to 
- * run a webserver, and instead of executing a query directly with the graphql function, we can use the graphql-http library to mount a GraphQL API 
- * server on the "/graphql" HTTP endpoint:
- */
-
 var express = require("express")
 var { createHandler } = require("graphql-http/lib/use/express")
 var { buildSchema } = require("graphql")
-var { ruruHTML } = require("ruru/server")
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
 	type Query {
-		hello: String
+		quoteOfTheDay: String
+		random: Float!
+		rollThreeDice: [Int]
 	}
 `)
 
-// The root provides a resolver function for each API endpoint		
+// The root provides a resolver function for each API endpoint
 var root = {
-	hello(){
-		return "Hello world!"
+	quoteOfTheDay(){
+		return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within"
+	},
+	random(){
+		return Math.random()
+	},
+	rollThreeDice(){
+		return [1,2,3].map(_ => 1 + Math.floor(Math.random() * 6))
 	},
 }
 
 var app = express()
-
-// Serve the GraphiQL IDE.
-app.get("/", (_req,res) => {
-	res.type("html")
-	res.end(ruruHTML({ endpoint: "/graphql" }))
-})
-
-// Create and use the GraphQL handler.
 app.all(
 	"/graphql",
 	createHandler({
@@ -38,7 +32,6 @@ app.all(
 		rootValue: root,
 	})
 )
-
-// Start the server at port 
 app.listen(4000)
-console.log("Running a GraphQL API Server at http://localhost:4000/graphql")
+console.log("Running a GraphQL API server at localhost:4000/graphql")
+
