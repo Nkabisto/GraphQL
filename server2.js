4c08b0a70aa09133b326b020e125d4e9a138745f
lionel <nkabisto@gmail.com>
2024-07-16 11:30:24 +0200

    Authentification and Middleware

---
+++ server2.js
@@ -0,0 +1,37 @@
+var express = require("express")
+var { createHandler } = require("graphql-http/lib/use/express")
+var { buildSchema } = require("graphql")
+
+var schema = buildSchema(`
+	type Query{
+		ip: String
+	}
+`)
+
+function loggingMiddleware(req,res,next){
+	console.log("ip:",req.ip)
+	next()
+}
+
+var root = {
+	ip(args, context){
+		return context.ip
+	},
+}
+
+var app = express()
+app.use(loggingMiddleware)
+app.all(
+	"https://localhost:4000/graphql",
+	createHandler({
+		schema: schema,
+		rootValue: root,
+		context: req => ({
+			ip: req.raw.ip,
+		}),
+	})
+)
+
+app.listen(4000)
+console.log("Running a GraphQL API server at localhost:4000/graphql")
+
